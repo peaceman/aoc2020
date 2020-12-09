67271308bf0a496570dfdefdef9ff6bcc4c2e21b
peaceman <nixalio@gmail.com>
2020-12-09 09:50:41 +0100

    Day 9 optimization

--- src/bin/day9b.rs
+++ src/bin/day9b.rs
@@ -31,69 +31,45 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<_>>();
 
     let preamble_length = opts.preamble_length;
+    let start = Instant::now();
     let invalid_number = find_invalid_number(&numbers, preamble_length);
-    let encryption_weakness = find_encryption_weakness(&numbers, invalid_number);
+    println!(
+        "invalid number: {}, elapsed: {:?}",
+        invalid_number,
+        Instant::now().duration_since(start)
+    );
 
-    println!("encryption weakness: {:?}", encryption_weakness);
+    let start = Instant::now();
+    let encryption_weakness = find_encryption_weakness(&numbers, invalid_number).unwrap();
+    println!(
+        "encryption weakness: {}, elapsed: {:?}",
+        encryption_weakness,
+        Instant::now().duration_since(start)
+    );
 
     Ok(())
 }
 
 fn find_invalid_number(numbers: &[u64], preamble_length: usize) -> u64 {
-    let start = Instant::now();
-    let sums = calculate_permutation_sums(&numbers);
+    for idx_num in preamble_length..numbers.len() {
+        let num = numbers[idx_num];
+        let mut number_is_valid = false;
 
-    let mut num = 0;
-    for idx in preamble_length..numbers.len() {
-        num = numbers[idx];
+        for idx_a in (idx_num - preamble_length)..idx_num {
+            let a = numbers[idx_a];
 
-        if !is_number_valid(num, idx - preamble_length, &sums) {
-            break;
-        }
-    }
-
-    println!(
-        "num: {:?}, elapsed: {:?}",
-        num,
-        Instant::now().duration_since(start)
-    );
-
-    num
-}
-
-fn calculate_permutation_sums(numbers: &[u64]) -> HashMap<u64, HashSet<(usize, usize)>> {
-    let mut sums = HashMap::new();
-
-    for (idx_a, v_a) in numbers.iter().enumerate() {
-        for (idx_b, v_b) in numbers.iter().enumerate() {
-            // skip same values
-            if v_a == v_b {
-                continue;
+            if numbers[(idx_a + 1)..idx_num].iter().any(|b| a + b == num) {
+                number_is_valid = true;
+                break;
             }
+        }
 
-            let sum = v_a + v_b;
-            sums.entry(sum)
-                .or_insert_with(HashSet::new)
-                .insert((idx_a, idx_b));
+        if !number_is_valid {
+            return num;
         }
     }
 
-    sums
-}
-
-fn is_number_valid(
-    num: u64,
-    first_valid_index: usize,
-    sums: &HashMap<u64, HashSet<(usize, usize)>>,
-) -> bool {
-    sums.get(&num)
-        .map(|sources| {
-            let valid_range = first_valid_index..;
-            sources
-                .iter()
-                .any(|indices| valid_range.contains(&indices.0) && valid_range.contains(&indices.1))
-        })
-        .unwrap_or(false)
+    return 0;
 }
 
 fn find_encryption_weakness(numbers: &[u64], invalid_number: u64) -> Option<u64> {
